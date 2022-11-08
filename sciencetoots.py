66c35de35392f385d30ca3df6fab9ec43ce1a175
Moreno Colaiacovo <emmecola83@gmail.com>
2022-11-08 19:16:59 +0100

    Update sciencetoots.py

--- sciencetoots.py
+++ sciencetoots.py
@@ -2,7 +2,7 @@
 """This script listens to public tweets mentioning "Send to @ScienceToots",
 it checks if the tweets are replies to another tweet or thread and send the
 replied tweets/threads to Mastodon via @sciencetoots@mstdn.science. An archive
-of the tweet IDs already sent prevents the bot to send multiple times the same
+of the tweet IDs already sent prevents the bot from sending multiple times the same
 tweet/thread. Images are also included in the messages sent to Mastodon.
 """
 
